7754bfebe553918e5f269d465e38db14adef4087
ali <alimotahari565@gmail.com>
2023-10-18 00:11:22 +0330

    add components

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,22 +1,14 @@
 import {createBrowserRouter} from "react-router-dom";
 import React from "react";
 import HomePage from "../pages/index.jsx";
-import AuthPage from "../pages/Auth.jsx";
-import ProfilePage from "../pages/Profile.jsx";
+
 
 const routes =[
     {
         path: "/",
         element: <HomePage/>,
     },
-    {
-        path: "/auth",
-        element: <AuthPage/>,
-    },
-    {
-        path: "/profile",
-        element: <ProfilePage/>,
-    },
+ 
 ];
 const router =  createBrowserRouter(routes)
 export default router;
