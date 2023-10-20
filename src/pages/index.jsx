60ce8fc51ce3ac7a3c09394f5e61168f9b6fb615
Mohammad javad Jafar Abady <mjafary822@gmail.com>
2023-10-20 20:36:03 +0330

    Create Date Picker

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import Button from '../components/Ui/Button';
+import DatePickerComponent from "../components/DatePicker/DatePickerComponent.jsx";
 
 function HomePage(props) {
     return (
@@ -8,6 +9,7 @@ function HomePage(props) {
             <h1>Pages:</h1>
             <Link to={'/profile'}><Button>Profile</Button></Link>
             <Link to={'/auth'}><Button>Auth</Button></Link>
+            <DatePickerComponent/>
         </div>
     );
 }
