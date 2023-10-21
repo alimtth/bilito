ddb5b8d44f9c8bdcf9c1de968d9b3a5c6ef8d0fe
Mohammad javad Jafar Abady <mjafary822@gmail.com>
2023-10-21 19:41:53 +0330

    create accordion

--- src/components/Ui/Input.jsx
+++ src/components/Ui/Input.jsx
@@ -4,9 +4,9 @@ import tickSquareButton from "/src/assets/tick-square-dark-button.svg"
 
 function Input() {
     return (
-            <div className='h-fit px-6 gap-2 rounded border border-gray-100 flex justify-between shrink-0 hover:shadow-xl hover:transition-shadow focus:border-blue-400 w-96 ' >
+            <div className='h-fit px-6 gap-2 rounded border border-gray-300 flex justify-between shrink-0 hover:shadow-xl hover:transition-shadow focus:border-blue-400 w-96 ' >
                 <img src={tickSquareButton} />
-                <input type="text" placeholder='عنوان' className='py-4 focus:outline-none text-right h-full flex-grow' />
+                <input type="text" placeholder='عنوان' className='py-4 focus:outline-none text-right h-full flex-grow'/>
                 <img src={tickSquareButton} />
             </div>
     )
