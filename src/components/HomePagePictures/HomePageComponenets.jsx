4cc4749041b87c6ecefd0f263b5f58bda5a3764a
matin-mahabadi <144549212+matin-mahabadi@users.noreply.github.com>
2023-10-23 15:26:41 +0330

    add component PopularTicked

--- src/components/HomePagePictures/HomePageComponenets.jsx
+++ src/components/HomePagePictures/HomePageComponenets.jsx
@@ -6,26 +6,26 @@ import kish from '../../assets/Images/Icons/HomePageKish.jpg'
 function HomePageComponenets() {
     return (
         <div className='flex justify-center gap-6'>
-            <div className='relative flex justify-start items-end  '>
+            <div className='rounded-lg overflow-hidden relative flex justify-start items-end  '>
                 <img src={kish} className='' />
                 <p className='absolute pb-11 pr-4 mr-2 mb-2 text-white text-xl '>بهترین فصل شنا</p>
                 <button className='h-auto w-auto mr-6 mb-2 border-2 p-2 absolute border-solid border-white rounded-lg text-white text-sm'>خرید بلیط پرواز های کیش</button>
                 
             </div>
-            <div className='relative flex justify-start items-end'>
+            <div className='relative rounded-lg overflow-hidden flex justify-start items-end'>
                 <img src={turkey} className=' ' />
                 <p className='absolute pb-11 pr-4 mr-2 mb-2 text-white text-xl '>سفر به ترکیه</p>
                 <button className='h-auto w-auto mr-6 mb-2 border-2 p-2 absolute border-solid border-white rounded-lg text-white text-sm'>خرید بلیط پرواز های ترکیه</button>
 
             </div>
             <div className='flex flex-col gap-6 '>
-                <div className='relative flex justify-start items-end'>
+                <div className='relative rounded-lg overflow-hidden flex justify-start items-end'>
                     <img src={dubai} className='' />
                     <p className='absolute pb-11 pr-4 mr-2 mb-2 text-white text-xl '>شگفتی در صحرا</p>
                     <button className='h-auto w-auto mr-6 mb-2 border-2 p-2 absolute border-solid border-white rounded-lg text-white text-sm'>خرید بلیط پرواز های دبی</button>
 
                 </div>
-                <div className='relative flex justify-start items-end'>
+                <div className='relative rounded-lg overflow-hidden flex justify-start items-end'>
                     <img src={shiraz} className='' />
                     <p className='absolute pb-11 pr-4 mr-2 mb-2 text-white text-xl '>دنیایی از تاریخ و هنر</p>
                     <button className='h-auto w-auto mr-6 mb-2 border-2 p-2 absolute border-solid border-white rounded-lg text-white text-sm'>خرید بلیط پرواز های شیراز</button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,12 @@ export default {
         gray:{
           100:'#ADADAD',
           200:'#F9F9F9',
-          300: '#DFDFDF'
+          300: '#DFDFDF',
+          400: '#EDEDED',
+          500: '#868686'
         },
         blue: {
+          100: '#E8F4FA',
           300:'#77BDE0',
           400:  '#E8F4FA',
           500: '#1D91CC',
@@ -25,6 +28,12 @@ export default {
             'lg': '48px',
             'xl': '56px',
     },
+       flex: {
+        2: '1 0 0',
+       },
+       fontSize: {
+        ssm: '10px',
+       }
     }
   },
   plugins: [],
