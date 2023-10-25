a34e2ab3cc6e25daeb8dbfa55f3b7e5ef75b9b5c
matin-mahabadi <144549212+matin-mahabadi@users.noreply.github.com>
2023-10-25 19:02:53 +0330

    edited router and fixed bugs

--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import Notf from '../../assets/Images/Icons/Component 137.png'
+import Notf from '@/assets/Images/Icons/Component 137.png'
 import { ArrowBackIos } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 
-function NotFuond() {
+function NotFound() {
     return (
         <div className='flex justify-center flex-col items-center '>
             <img src={Notf} alt="404" className=' w-[708px]'/>
             <Link to={"/"}>
-            <button  className='mt-[30px] text-blue-650'>برگشت به صفحه اصلی<ArrowBackIos /></button>
+             <button  className='mt-[30px] text-blue-650'>برگشت به صفحه اصلی<ArrowBackIos /></button>
             </Link>
         </div>
     );
 }
 
-export default NotFuond;
+export default NotFound;
