93f87f5b1f431fbb62be0c67d46c8994e8a6640e
ali <alimotahari565@gmail.com>
2023-10-27 12:50:28 +0330

    fix parent div ticketList

--- src/pages/PageTicketList/TickedList.jsx
+++ src/pages/PageTicketList/TickedList.jsx
@@ -9,22 +9,15 @@ import Button from "@/components/Ui/Button";
 import { ArrowUpward } from "@mui/icons-material";
 import { Slider } from "@mui/material";
 import airport from "@/assets/Images/Icons/Airport.png";
-import { IoIosArrowDown } from "react-icons/io"
-import airplane from '@/assets/Images/Icons/airplaneBorder.svg'
-import calendar from '@/assets/Images/Icons/calendar.svg'
-import profileAdd from '@/assets/Images/Icons/profile-add.svg'
-import EditPen from '@/assets/Images/Icons/edit.svg'
-import Button from '@/components/Ui/Button';
 import TicketList from "@/components/TicketList/index.jsx";
 
 
 function TickedList() {
   return (
-    <div className=" flex flex-col items-center">
+        <div className=" flex flex-col items-center">
       <section className="flex flex-col relative items-center">
         <img src={titlePic} />
       </section>
-
       <section className="-m-12 z-10 bg-white flex flex-col items-center rounded-lg shadow-2xl">
         <div className="p-6 gap-8 flex flex-shrink-0 justify-center items-center  font-bold">
           <div className="gap-2 flex items-center">
@@ -52,8 +45,8 @@ function TickedList() {
         </div>
       </section>
 
-      <div className="flex mt-16 ">
-        <div className="w-[288px] bg-gray-400 rounded-[8px] basis-[25%] p-[24px] ml-[25px]">
+    <div className="flex mt-20 ">
+      <div className="w-[288px] bg-gray-400 rounded-[8px] basis-[25%] p-[24px] ml-[25px]">
           <div className="flex justify-between">
             <h3>تعداد نتایج: 121</h3>
             <h3 className="text-blue-500 text-[13px]"> پاک کردن فیلتر‌ها</h3>
@@ -77,19 +70,6 @@ function TickedList() {
           <div className="flex justify-between mt-[16px]">
             <h4>زمان حرکت</h4>
             <IoIosArrowDown />
-      <div className='flex basis-3/4 flex-col'>
-        <div className='mt-20 gap-8 flex flex-row '>
-          <div className='gap-2 py-2 px-4 flex flex-shrink-0 flex-col items-start border border-gray-100 rounded-lg hover:border-blue-500 hover:text-blue-500 w-[745px] '>
-            <div className='flex justify-between items-center self-stretch '>
-              <p>تقویم قیمتی</p>
-              <IoIosArrowDown />
-            </div>
-          </div>
-          <div className='gap-2 py-2 px-6 flex  justify-center items-center border border-gray-100 rounded-lg hover:border-blue-500 hover:text-blue-500 '>
-            <div className='flex  justify-between items-center '>
-              <p>متن ورودی</p>
-              <IoIosArrowDown />
-            </div>
           </div>
           <div className="flex justify-between mt-[10px]">
             <h4>24:00</h4>
@@ -199,7 +179,7 @@ function TickedList() {
             <hr className="mt-[16px] bg-gray-500 w-full h-[2px]" />
           </div>
         </div>
-        <div className="flex-auto">
+        <div className="flex-auto basis-[75%]">
           <div className="flex gap-4">
             <div className="gap-2 py-2 px-4 flex justify-between border border-gray-100 rounded-lg hover:border-blue-500 hover:text-blue-500 w-[745px] ">
               <div className="flex justify-between items-center self-stretch ">
@@ -215,10 +195,10 @@ function TickedList() {
               </div>
             </div>
           </div>
+         <TicketList/>
         </div>
-        <TicketList/>
-      </div>
-    </div>
+        </div>
+       </div>
   );
 }
 
