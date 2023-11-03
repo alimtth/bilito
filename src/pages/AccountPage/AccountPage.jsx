dc052f973a2677f853828571c09c1a09aa09a4f3
ali <alimotahari565@gmail.com>
2023-11-03 12:23:17 +0330

    add edit user

--- src/pages/AccountPage/AccountPage.jsx
+++ src/pages/AccountPage/AccountPage.jsx
@@ -10,15 +10,21 @@ import like from "@/assets/Images/Icons/like.png"
 import dislike from "@/assets/Images/Icons/dislike.png"
 import profile from "@/assets/Images/Icons/profile-add.svg"
 import { useAuthContext } from '@/providers/AuthProvider';
+import InputTextField from '@/components/Ui/InputTextField';
 
 const AccountPage = () => {
     const { currentUser, logout } = useAuthContext();
     const [selected, setSelected] = useState(false);
+    const [editUser, setEditUser] = useState(false);
 
     const handlChangSelected = () => {
         setSelected(!selected)
     }
-    console.log(selected);
+
+    const handlEdit = () => {
+        setEditUser(true);
+    }
+    console.log(editUser);
     return (
         <div>
             <HeaderTabs />
@@ -28,38 +34,66 @@ const AccountPage = () => {
                         <div className="flex flex-col gap-1 items-center px-28 py-5">
                             <img src={currentUser?.avatar} alt="" className='rounded-full w-48' />
                             <h4 className='mt-4 font-bold text-gray-500'>علی مطهری</h4>
-                            <p className='text-gray-500'>09121111020</p>
+                            <p className='text-gray-500'>۰۹‍۱۲۱۱۱۰۱۱۰</p>
                         </div>
                         <div className="flex flex-col items-start gap-7 w-full mr-2">
                             <hr className='w-full bg-gray-20' />
                             <p className={`flex items-center text-gray-600 gap-3 cursor-pointer w-72 h-12 rounded-md ${selected === true ? 'bg-blue-100' : ''}`} onClick={handlChangSelected}><img src={profile} alt="" />اطلاعات حساب کاربری</p>
-                            
-                            {/* <p className={`flex items-center text-gray-600 gap-3 cursor-pointer w-72 h-12 rounded-md ${selected === 1 ? 'bg-blue-100' : ''}`} onClick={handlChangSelected}><img src={profile} alt="" />سفرهای من</p> */}
-                            {/* <p className={`flex items-center text-gray-600 gap-3 cursor-pointer w-72 h-12 rounded-md ${selected === 2 ? 'bg-blue-100' : ''}`} onClick={handlChangSelected}><img src={profile} alt="" />تیکت‌های من</p> */}
-                            {/* <p className={`flex items-center text-gray-600 gap-3 cursor-pointer w-72 h-12 rounded-md ${selected === 3 ? 'bg-blue-100' : ''}`} onClick={handlChangSelected}><img src={profile} alt="" />کیف پول</p> */}
-                            {/* <p className={`flex items-center text-red-600 gap-3 mb-6 cursor-pointer w-72 h-12 rounded-md ${selected === 4 ? 'bg-blue-100' : ''}`} onClick={logout}><img src={profile} alt="" />خروج از حساب کاربری</p> */}
+
+                            <p className={`flex items-center text-gray-600 gap-3 cursor-pointer w-72 h-12 rounded-md ${selected === 1 ? 'bg-blue-100' : ''}`} onClick={handlChangSelected}><img src={profile} alt="" />سفرهای من</p>
+                            <p className={`flex items-center text-gray-600 gap-3 cursor-pointer w-72 h-12 rounded-md ${selected === 2 ? 'bg-blue-100' : ''}`} onClick={handlChangSelected}><img src={profile} alt="" />تیکت‌های من</p>
+                            <p className={`flex items-center text-gray-600 gap-3 cursor-pointer w-72 h-12 rounded-md ${selected === 3 ? 'bg-blue-100' : ''}`} onClick={handlChangSelected}><img src={profile} alt="" />کیف پول</p>
+                            <p className={`flex items-center text-red-600 gap-3 mb-6 cursor-pointer w-72 h-12 rounded-md ${selected === 4 ? 'bg-blue-100' : ''}`} onClick={logout}><img src={profile} alt="" />خروج از حساب کاربری</p>
                         </div>
                     </div>
                     {selected ? (
+                        editUser ? (
+                            <div className='flex flex-col w-full mt-16 '>
+                            <div className="flex justify-between items-center ">
+                                <h3 className="font-bold">ویرایش اطلاعات</h3>
+                            </div>
+                            <div className="outline p-5 outline-gray-400 rounded-lg mt-7  ">
+                                <div className="flex justify-between">
+                                    <InputTextField size={"xsl"}>
+                                    نام و نام خانوداگی
+                                    </InputTextField>
+                                    <InputTextField size={"xsl"}>
+                                    جنسیت
+                                    </InputTextField>
+                                </div>
+                                <div className="flex justify-between mt-8">
+                                    <InputTextField size={"xsl"} >
+                                    کد ملی
+                                    </InputTextField>
+                                    <InputTextField size={"xsl"}>
+                                    شماره موبایل
+                                    </InputTextField>
+                                </div>
+                                <div className='flex justify-end mt-5'>
+                                <Buttons variant='fill' className={"w-36 flex justify-center items-end" }>ثبت</Buttons>
+                                </div>
+                            </div>
+                        </div>
+                        ):(
                         <div className='flex flex-col w-full mt-16 '>
                             <div className="flex justify-between items-center ">
                                 <h3 className="font-bold">اطلاعات حساب کاربری</h3>
                             </div>
                             <div className="outline p-5 outline-gray-400 rounded-lg mt-7  ">
                                 <div className="flex justify-between">
-                                    
-                                    <img src={deletesss} alt="" />
+                                    <p className="flex gap-20 justify-center items-center text-gray-500">نام و نام خانوادگی<p>جنسیت</p></p>
+                                    <Buttons className={"outline-none border-none"} onClick={handlEdit}>ویرایش اطلاعات</Buttons>
                                 </div>
-                                <h2 className='text-gray-500 mt-3'>چگونه می‌توانم از سایت شما بلیط اوکراین را رزرو کنم؟</h2>
-                                <div className="flex justify-between mt-5">
-                                    <p className="flex gap-3"><img src={undo} alt="" />با سلام و وقت بخیر. لطفا به قسمت راهنمای تهیه بلیط در بخش هدر مراجعه نمایید.</p>
-                                    <div className='flex justify-center items-center gap-2'>
-                                        <img src={dislike} alt="" />
-                                        <img src={like} alt="" />
-                                    </div>
+                                <div className="flex flex-col">
+                                    <h2 className='mt-3 flex gap-32 text-lg text-black'>علی مطهری<h2>مرد</h2></h2>
+                                    <p className="flex gap-36  text-gray-500 mt-10">کد ملی<p>شماره تماس</p></p>
+
+
+                                    <h2 className='mt-3 flex gap-20 text-lg text-black'>۰۳۷۲۹۴۴۳۱۰ <h2>۰۹‍۱۲۱۱۱۰۱۱۰</h2></h2>
                                 </div>
                             </div>
                         </div>
+                        )
                     ) : (
                         <div className='flex flex-col w-full mt-11 '>
                             <div className="flex justify-between items-center ">
