39bf03e9f13375eb16276f56c6c110ceeef46c03
ali <alimotahari565@gmail.com>
2023-11-06 12:29:59 +0330

    fix route accont

--- src/pages/AccountPage/BaseLayoutsAccount/BaseLayoutAcc.jsx
+++ src/pages/AccountPage/BaseLayoutsAccount/BaseLayoutAcc.jsx
@@ -1,14 +1,17 @@
+import HeaderTabs from '@/components/NavBar/HeaderTabs';
 import { useAuthContext } from '@/providers/AuthProvider';
 import React from 'react';
+import { Link, Outlet, redirect } from 'react-router-dom';
 import profile from "@/assets/Images/Icons/profile-add.svg"
-import HomePageComponenets from '@/components/HomePagePictures/HomePageComponenets';
-
 
 function BaseLayoutAcc() {
     const { currentUser, logout } = useAuthContext();
     return (
         <div>
-            <div className="outline  outline-gray-400 rounded-lg mt-28">
+            <HeaderTabs />
+            <div id="container">
+                <div className="flex">
+                    <div className="outline  outline-gray-400 rounded-lg mt-28">
                         <div className="flex flex-col gap-1 items-center px-28 py-5">
                             <img src={currentUser?.avatar} alt="" className='rounded-full w-48' />
                             <h4 className='mt-4 font-bold text-gray-500'>علی مطهری</h4>
@@ -16,15 +19,23 @@ function BaseLayoutAcc() {
                         </div>
                         <div className="flex flex-col items-start gap-7 w-full mr-2">
                             <hr className='w-full bg-gray-20' />
-                            <p className={`flex items-center text-gray-600 gap-3 cursor-pointer w-72 h-12 rounded-md `} ><img src={profile} alt="" />اطلاعات حساب کاربری</p>
-
-                            {/* <p className={`flex items-center text-gray-600 gap-3 cursor-pointer w-72 h-12 rounded-md `} ><img src={profile} alt="" />سفرهای من</p>
+                            <Link to={"datauser"}>
+                                <p className={`flex items-center text-gray-600 gap-3 cursor-pointer w-72 h-12 rounded-md `} ><img src={profile} alt="" />اطلاعات حساب کاربری</p>
+                            </Link>
+                            <p className={`flex items-center text-gray-600 gap-3 cursor-pointer w-72 h-12 rounded-md `} ><img src={profile} alt="" />سفرهای من</p>
+                            <Link to={"myticket"}>
                             <p className={`flex items-center text-gray-600 gap-3 cursor-pointer w-72 h-12 rounded-md `} ><img src={profile} alt="" />تیکت‌های من</p>
-                            <p className={`flex items-center text-gray-600 gap-3 cursor-pointer w-72 h-12 rounded-md `} src={profile} alt="" />کیف پول</p>
-                            <p className={`flex items-center text-red-600 gap-3 mb-6 cursor-pointer w-72 h-12 rounded-md ''}`} <img src={profile} alt="" />خروج از حساب کاربری</p> */}
+                            </Link>
+                            <p className={`flex items-center text-gray-600 gap-3 cursor-pointer w-72 h-12 rounded-md `}><img src={profile} alt=""/> کیف پول</p>
+                            <p className={`flex items-center text-red-600 gap-3 mb-6 cursor-pointer w-72 h-12 rounded-md ''}`} onClick={logout}> <img src={profile} alt=""  />خروج از حساب کاربری</p>
                         </div>
-                        
-                    </div>  
+
+                    </div>
+                </div>
+            </div>
+            <main>
+                <Outlet/>
+            </main>
         </div>
     );
 }
---
+++ src/pages/AccountPage/EditUserData/EditUserData.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import BaseLayoutAcc from '../BaseLayoutsAccount/BaseLayoutAcc';
+import InputTextField from '@/components/Ui/InputTextField';
+import Buttons from '@/components/Ui/Button'
+
+function EditUserData() {
+    return (
+        <div>
+            <div id="container">
+                    <div className='flex flex-col w-full mt-16 '>
+                            <div className="flex justify-between items-center ">
+                                <h3 className="font-bold">ویرایش اطلاعات</h3>
+                            </div>
+                            <div className="outline p-5 outline-gray-400 rounded-lg mt-7  ">
+                                <div className="flex justify-between">
+                                    <InputTextField size={"xsl"}>
+                                    نام و نام خانوادگی
+                                    </InputTextField>
+                                    <InputTextField size={"xsl"}>
+                                    جنسیت
+                                    </InputTextField>
+                                </div>
+                                <div className="flex justify-between mt-8">
+                                    <InputTextField size={"xsl"} >
+                                    کد ملی
+                                    </InputTextField>
+                                    <InputTextField size={"xsl"}>
+                                    شماره موبایل
+                                    </InputTextField>
+                                </div>
+                                <div className='flex justify-end mt-5'>
+                                <Buttons variant='fill' className={"w-36 flex justify-center items-end" }>ثبت</Buttons>
+                                </div>
+                            </div>
+                        </div>
+            </div>
+        </div>
+    );
+}
+
+export default EditUserData;
