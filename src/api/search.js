d7964f4137efb3feb79a2d05fcaa76c4502272d9
Ali Motahari <alimotahari565@gmail.com>
2023-11-08 00:30:51 +0330

    change search in ticekt list (#49)

---
+++ src/api/search.js
@@ -0,0 +1,16 @@
+import { sampleTicket } from "@/FakeData/ticket";
+import { fakeRequest } from "@/FakeData/utils";
+
+const apiSearch = async (filters) => {
+
+    var data = sampleTicket;
+
+    if(filters?.q) {
+        data = sampleTicket.filter((post) => post.forth.includes(filters.q));
+    }
+    const result = await fakeRequest(data)
+
+    return result;
+}
+
+export { apiSearch };
