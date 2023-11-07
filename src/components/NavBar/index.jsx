e257ae7a3f96b6c1dc16af826f83ee9b97079a2e
matin-mahabadi <144549212+matin-mahabadi@users.noreply.github.com>
2023-11-07 22:58:00 +0330

    edite navbar

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react' 
+import HeaderTabs from './HeaderTabs';
+import HomePageScreen from './HomePageScreen';
+
+function NavBar() {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default NavBar
