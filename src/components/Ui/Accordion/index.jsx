831b8c431797a18d4b8f0da858d3fefa96c799c1
Mohammad javad Jafar Abady <mjafary822@gmail.com>
2023-11-08 22:37:40 +0330

    config accordionData

--- src/components/Ui/Accordion/index.jsx
+++ src/components/Ui/Accordion/index.jsx
@@ -6,6 +6,7 @@ import MuiAccordionSummary from '@mui/material/AccordionSummary';
 import MuiAccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import './accordion.css'
+import {accordionData} from "@/FakeData/accordionData.js";
 
 const Accordion = styled((props) => (
     <MuiAccordion disableGutters elevation={0} square {...props} />
@@ -53,112 +54,111 @@ export default function CustomizedAccordions() {
     return (
         <div id='container' className='pt-24'>
             <h1 className='text-xl font-bold mb-8'>سوالات متداول</h1>
-            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-                <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-                    <div className={`${expanded === 'panel1' ? 'text-blue-500' : ''}`}>
-                        <Typography className='font1'>در هر پرواز میزان بار مجاز چقدر است؟</Typography>
-                    </div>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography className='font1'>
-                        بلیط تمام خطوط هوایی دنیا در سایت بیلیتو موجود است، چه پروازهایی که مبدا یا مقصد آنها ایران است
-                        و چه پروازهای داخلی دورترین کشورهای دنیا. پروازهای ایرلاین‌هایی مثل لوفت‌هانزا، امارات،
-                        قطرایرویز، ترکیش‌ایر، ایرفرانس، کی‌ال‌ام، آئروفلوت، آلیتالیا، اوکراینی، ایرایژیا، پگاسوس و ده‌ها
-                        ایرلاین دیگر در بیلیتو قابل تهیه هستند. همچنین بلیط پروازهای خارجیِ شرکت‌های هواپیمایی داخلی
-                        مانند ماهان، ایران‌ایر، قشم ایر، آتا و... نیز روی سایت بیلیتو به فروش می‌رسد.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-                <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
-                    <div className={`${expanded === 'panel2' ? 'text-blue-500' : ''}`}>
-                        <Typography className='font1'>نرخ بلیط هواپیما برای نوزادان و کودکان زیر 12سال چگونه است؟</Typography>
-                    </div>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography className='font1'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-                        sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                        sit amet blandit leo lobortis eget.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-                <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
-                    <div className={`${expanded === 'panel3' ? 'text-blue-500' : ''}`}>
-                        <Typography className='font1'>آیا پس از خرید اینترنتی بلیط هواپیما امکان استرداد آن وجود دارد؟</Typography>
-                    </div>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography className='font1'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-                        sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                        sit amet blandit leo lobortis eget.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
-                <AccordionSummary aria-controls="panel4d-content" id="panel4d-header">
-                    <div className={`${expanded === 'panel4' ? 'text-blue-500' : ''}`}>
-                        <Typography className='font1'>آیا پس از خرید بلیط هواپیما امکان تغییر نام یا نام خانوادگی وجود دارد؟</Typography>
-                    </div>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography className='font1'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-                        sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                        sit amet blandit leo lobortis eget.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion expanded={expanded === 'panel5'} onChange={handleChange('panel5')}>
-                <AccordionSummary aria-controls="panel5d-content" id="panel5d-header">
-                    <div className={`${expanded === 'panel5' ? 'text-blue-500' : ''}`}>
-                        <Typography className='font1'>هنگامی که از سایت خرید بلیط هواپیما رزرو بلیط را انجام می‌دهیم امکان انتخاب صندلی مورد نظرمان وجود دارد؟</Typography>
-                    </div>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography className='font1'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-                        sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                        sit amet blandit leo lobortis eget.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion expanded={expanded === 'panel6'} onChange={handleChange('panel6')}>
-                <AccordionSummary aria-controls="panel6d-content" id="panel6d-header">
-                    <div className={`${expanded === 'panel6' ? 'text-blue-500' : ''}`}>
-                        <Typography className='font1'>بلیط پرواز چه کشورها ایرلاین‌هایی را می‌توانم‌ در سایت بیلیتو جستجو و خریداری کنم؟</Typography>
-                    </div>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography className='font1'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-                        sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                        sit amet blandit leo lobortis eget.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion expanded={expanded === 'panel7'} onChange={handleChange('panel7')}>
-                <AccordionSummary aria-controls="panel7d-content" id="panel7d-header">
-                    <div className={`${expanded === 'panel7' ? 'text-blue-500' : ''}`}>
-                        <Typography className='font1'>چطور تاریخ پرواز را تغییر دهیم؟</Typography>
-                    </div>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography className='font1'>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor
-                        sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-                        sit amet blandit leo lobortis eget.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
+            {accordionData.map((e)=>{
+                return (<Accordion expanded={expanded === e.id} onChange={handleChange(e.id)}>
+                    <AccordionSummary aria-controls={`${e.id}d-content`} id={`${e.id}d-header`}>
+                        <div className={`${expanded === e.id ? 'text-blue-500' : ''}`}>
+                            <Typography className='font1'>{e.title}</Typography>
+                        </div>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography className='font1'>
+                            {e.description}
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>)
+            })}
+
+            {/*<Accordion expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>*/}
+            {/*    <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">*/}
+            {/*        <div className={`${expanded === 'panel2' ? 'text-blue-500' : ''}`}>*/}
+            {/*            <Typography className='font1'>نرخ بلیط هواپیما برای نوزادان و کودکان زیر 12سال چگونه است؟</Typography>*/}
+            {/*        </div>*/}
+            {/*    </AccordionSummary>*/}
+            {/*    <AccordionDetails>*/}
+            {/*        <Typography className='font1'>*/}
+            {/*            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse*/}
+            {/*            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor*/}
+            {/*            sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,*/}
+            {/*            sit amet blandit leo lobortis eget.*/}
+            {/*        </Typography>*/}
+            {/*    </AccordionDetails>*/}
+            {/*</Accordion>*/}
+            {/*<Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>*/}
+            {/*    <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">*/}
+            {/*        <div className={`${expanded === 'panel3' ? 'text-blue-500' : ''}`}>*/}
+            {/*            <Typography className='font1'>آیا پس از خرید اینترنتی بلیط هواپیما امکان استرداد آن وجود دارد؟</Typography>*/}
+            {/*        </div>*/}
+            {/*    </AccordionSummary>*/}
+            {/*    <AccordionDetails>*/}
+            {/*        <Typography className='font1'>*/}
+            {/*            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse*/}
+            {/*            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor*/}
+            {/*            sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,*/}
+            {/*            sit amet blandit leo lobortis eget.*/}
+            {/*        </Typography>*/}
+            {/*    </AccordionDetails>*/}
+            {/*</Accordion>*/}
+            {/*<Accordion expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>*/}
+            {/*    <AccordionSummary aria-controls="panel4d-content" id="panel4d-header">*/}
+            {/*        <div className={`${expanded === 'panel4' ? 'text-blue-500' : ''}`}>*/}
+            {/*            <Typography className='font1'>آیا پس از خرید بلیط هواپیما امکان تغییر نام یا نام خانوادگی وجود دارد؟</Typography>*/}
+            {/*        </div>*/}
+            {/*    </AccordionSummary>*/}
+            {/*    <AccordionDetails>*/}
+            {/*        <Typography className='font1'>*/}
+            {/*            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse*/}
+            {/*            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor*/}
+            {/*            sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,*/}
+            {/*            sit amet blandit leo lobortis eget.*/}
+            {/*        </Typography>*/}
+            {/*    </AccordionDetails>*/}
+            {/*</Accordion>*/}
+            {/*<Accordion expanded={expanded === 'panel5'} onChange={handleChange('panel5')}>*/}
+            {/*    <AccordionSummary aria-controls="panel5d-content" id="panel5d-header">*/}
+            {/*        <div className={`${expanded === 'panel5' ? 'text-blue-500' : ''}`}>*/}
+            {/*            <Typography className='font1'>هنگامی که از سایت خرید بلیط هواپیما رزرو بلیط را انجام می‌دهیم امکان انتخاب صندلی مورد نظرمان وجود دارد؟</Typography>*/}
+            {/*        </div>*/}
+            {/*    </AccordionSummary>*/}
+            {/*    <AccordionDetails>*/}
+            {/*        <Typography className='font1'>*/}
+            {/*            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse*/}
+            {/*            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor*/}
+            {/*            sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,*/}
+            {/*            sit amet blandit leo lobortis eget.*/}
+            {/*        </Typography>*/}
+            {/*    </AccordionDetails>*/}
+            {/*</Accordion>*/}
+            {/*<Accordion expanded={expanded === 'panel6'} onChange={handleChange('panel6')}>*/}
+            {/*    <AccordionSummary aria-controls="panel6d-content" id="panel6d-header">*/}
+            {/*        <div className={`${expanded === 'panel6' ? 'text-blue-500' : ''}`}>*/}
+            {/*            <Typography className='font1'>بلیط پرواز چه کشورها ایرلاین‌هایی را می‌توانم‌ در سایت بیلیتو جستجو و خریداری کنم؟</Typography>*/}
+            {/*        </div>*/}
+            {/*    </AccordionSummary>*/}
+            {/*    <AccordionDetails>*/}
+            {/*        <Typography className='font1'>*/}
+            {/*            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse*/}
+            {/*            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor*/}
+            {/*            sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,*/}
+            {/*            sit amet blandit leo lobortis eget.*/}
+            {/*        </Typography>*/}
+            {/*    </AccordionDetails>*/}
+            {/*</Accordion>*/}
+            {/*<Accordion expanded={expanded === 'panel7'} onChange={handleChange('panel7')}>*/}
+            {/*    <AccordionSummary aria-controls="panel7d-content" id="panel7d-header">*/}
+            {/*        <div className={`${expanded === 'panel7' ? 'text-blue-500' : ''}`}>*/}
+            {/*            <Typography className='font1'>چطور تاریخ پرواز را تغییر دهیم؟</Typography>*/}
+            {/*        </div>*/}
+            {/*    </AccordionSummary>*/}
+            {/*    <AccordionDetails>*/}
+            {/*        <Typography className='font1'>*/}
+            {/*            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse*/}
+            {/*            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor*/}
+            {/*            sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,*/}
+            {/*            sit amet blandit leo lobortis eget.*/}
+            {/*        </Typography>*/}
+            {/*    </AccordionDetails>*/}
+            {/*</Accordion>*/}
         </div>
     );
 }
