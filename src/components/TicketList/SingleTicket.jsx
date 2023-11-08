d5ce388766212ee59b06e74225625331315064d2
ali <alimotahari565@gmail.com>
2023-11-08 11:09:54 +0330

    add modal in py

--- src/components/TicketList/SingleTicket.jsx
+++ src/components/TicketList/SingleTicket.jsx
@@ -7,13 +7,22 @@ import iconbag from '@/assets/Images/Icons/bag.svg'
 import line from '@/assets/Images/Icons/Line.svg'
 import propTypes from "prop-types";
 import { Link } from 'react-router-dom';
-function SingleTicket({forth, back, children}) {
+import close from "@/assets/Images/Icons/close-square.svg"
+function SingleTicket({ forth, back, children }) {
     const [isloding, setIsloding] = useState(true);
+    const [isShow, setIsShow] = useState(false);
+
+    const handlShow = () => {
+        setIsShow(true)
+    }
+    const handlclose = () => {
+        setIsShow(false)
+    }
 
     setTimeout(() => {
         setIsloding(false)
-    }, 5000);
-    
+    }, 2000);
+
     return (
         <>
             <div className='flex border-gray-10 border rounded-lg flex-col gap-4 mt-6 justify-evenly  '>
@@ -43,73 +52,127 @@ function SingleTicket({forth, back, children}) {
                     </div>
                 ) : (
                     // forth.map((item , forth, back) => (
-                        <div>
-                            <div className='flex justify-between items-center px-4 lg:flex-row flex-col gap-4 py-6'>
-                                <div className='flex justify-center items-center gap-2' >
-                                    <div
-                                        className='bg-red-10 text-red-20 flex justify-center items-center gap-1 py-[2px] px-2 h-6 border rounded-md'>
-                                        صندلی باقی مانده
-                                    </div>
-                                    <div
-                                        className='bg-red-10 text-red-20 flex justify-center items-center gap-1 py-[2px] px-2 h-6 border rounded-md'>غیر
-                                        قابل استرداد
+                    <div>
+                        <div className='flex justify-between items-center px-4 lg:flex-row flex-col gap-4 py-6'>
+                            <div className='flex justify-center items-center gap-2' >
+                                <div
+                                    className='bg-red-10 text-red-20 flex justify-center items-center gap-1 py-[2px] px-2 h-6 border rounded-md'>
+                                    صندلی باقی مانده
+                                </div>
+                                <div
+                                    className='bg-red-10 text-red-20 flex justify-center items-center gap-1 py-[2px] px-2 h-6 border rounded-md'>غیر
+                                    قابل استرداد
+                                </div>
+                            </div>
+                            <div className='flex justify-center items-center gap-[8px]'>
+                                <div
+                                    className='bg-blue-100 text-blue-500  flex justify-center items-center gap-1 py-[2px] px-2 h-6 border rounded-md'>اکونومی
+                                </div>
+                                <div
+                                    className='bg-blue-100 text-blue-500  flex justify-center items-center gap-1 py-[2px] px-2 h-6 border rounded-md'>سیستمی
+                                </div>
+                            </div>
+                        </div>
+                        <div className='flex justify-between items-end gap-[64px] px-4 lg:flex-row flex-col'>
+                            <div className='pb-5 gap-6 w-[575px] flex items-center '>
+                                <img src={omanAir} />
+                                <div>
+                                    <div className='pl-8 gap-2 flex flex-col items-center'>
+                                        <p>02:50</p>
+                                        <p className='text-gray-600'>{forth}(SAW)</p>
                                     </div>
                                 </div>
-                                <div className='flex justify-center items-center gap-[8px]'>
+
+                                <div className='flex items-center justify-center'>
+                                    <div className='gap-6 flex flex-col'>
+                                        <div className='gap-[2px] flex items-center justify-center'>
+                                            <img src={iconetimer} />
+                                            <p className='text-gray-500 '>19:00</p>
+                                        </div>
+                                        <div className='relative flex items-center justify-center '>
+                                            <img className='z-20' src={iconeairplan} />
+                                            <span className='absolute w-[142px] h-[1px] fill-none z-10'>
+                                                <img src={line} />
+                                            </span>
+                                        </div>
+                                        <div className='gap-[2px] flex items-center justify-center'>
+                                            <img src={iconbag} />
+                                            <p className='text-gray-500'>20Kg</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className='pr-8 gap-2 flex flex-col items-center'>
+                                    <p>21:50</p>
+                                    <p className='text-gray-600'>{back}(DXB)</p>
+                                </div>
+                            </div>
+                            <div className='flex lg:justify-center lg:items-end gap-6 items-center self-center'>
+                                <div className='text-blue-500'> تومان</div>
+                                <div>
+                                    {/* <Link to={"/form"}> */}
+                                    <Button size='lg' onClick={handlShow}>جزئیات بلیط</Button>
+                                    {/* </Link> */}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    // ))
+                )}
+                {isShow && (
+                    <div className="fixed inset-0 flex items-center justify-center z-50 bg-gray-600 bg-opacity-70">
+                        <div className=" bg-white  w-[800px] rounded-lg  shadow-lg z-50 py-3 px-3">
+                            <div className="flex justify-end">
+                                <img src={close} alt="" onClick={handlclose} />
+                            </div>
+                            <div className="flex gap-6">
+                                <ul>اطلاعات پرواز</ul>
+                                <ul>قوانین استرداد</ul>
+                                <ul>قوانین ویزا و مسیر</ul>
+                                <ul>بار مجاز</ul>
+                            </div>
+                            <hr className='w-full bg-gray-400 h-0.5 mt-3 mb-5' />
+                            <div className="flex flex-col">
+                                <div className="flex justify-between items-center">
+                                    <h3 className='flex justify-center '><img src={omanAir} alt="" className='w-7' />gulf air</h3>
+                                    <p>مجموعا 19:00 ساعت</p>
+                                    <div className='flex flex-col gap-3'>
+                                        <div
+                                            className='bg-red-10 text-red-20 flex justify-center items-center gap-1 py-[2px] px-2 h-6 border rounded-md'>
+                                            صندلی باقی مانده
+                                        </div>
+                                        <div
+                                            className='bg-red-10 text-red-20 flex justify-center items-center gap-1 py-[2px] px-2 h-6 border rounded-md'>غیر
+                                            قابل استرداد
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="flex justify-between items-center mt-6">
+                                    <div className='flex gap-3'>
+                                        <h3>02:50</h3>
+                                        <p>استانبول، فرودگاه استانبول</p>
+                                        <p>دوشنبه 6 شهریور (شماره پرواز: 615)</p>
+                                    </div>
+                                    <div className='flex gap-2'>
                                     <div
                                         className='bg-blue-100 text-blue-500  flex justify-center items-center gap-1 py-[2px] px-2 h-6 border rounded-md'>اکونومی
                                     </div>
                                     <div
                                         className='bg-blue-100 text-blue-500  flex justify-center items-center gap-1 py-[2px] px-2 h-6 border rounded-md'>سیستمی
                                     </div>
-                                </div>
-                            </div>
-                            <div className='flex justify-between items-end gap-[64px] px-4 lg:flex-row flex-col'>
-                                <div className='pb-5 gap-6 w-[575px] flex items-center '>
-                                    <img src={omanAir} />
-                                    <div>
-                                        <div className='pl-8 gap-2 flex flex-col items-center'>
-                                            <p>02:50</p>
-                                            <p className='text-gray-600'>{forth}(SAW)</p>
-                                        </div>
-                                    </div>
-
-                                    <div className='flex items-center justify-center'>
-                                        <div className='gap-6 flex flex-col'>
-                                            <div className='gap-[2px] flex items-center justify-center'>
-                                                <img src={iconetimer} />
-                                                <p className='text-gray-500 '>19:00</p>
-                                            </div>
-                                            <div className='relative flex items-center justify-center '>
-                                                <img className='z-20' src={iconeairplan} />
-                                                <span className='absolute w-[142px] h-[1px] fill-none z-10'>
-                                                    <img src={line} />
-                                                </span>
-
-                                            </div>
-                                            <div className='gap-[2px] flex items-center justify-center'>
-                                                <img src={iconbag} />
-                                                <p className='text-gray-500'>20Kg</p>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div className='pr-8 gap-2 flex flex-col items-center'>
-                                        <p>21:50</p>
-                                        <p className='text-gray-600'>{back}(DXB)</p>
                                     </div>
                                 </div>
-                                <div className='flex lg:justify-center lg:items-end gap-6 items-center self-center'>
-                                    <div className='text-blue-500'> تومان</div>
-                                    <div>
-                                        <Link to={"/form"}>
-                                        <Button size='lg'>جزئیات بلیط</Button>
-                                        </Link>
-                                    </div>
+                                <div className="flex">
+                                <div className=' flex items-center justify-center rotate-90 mt-20'>
+                                            <img className='z-10' src={iconeairplan} />
+                                            <span className='absolute w-[142px] h-[1px] '>
+                                                <img src={line} />
+                                            </span>
+                                        </div>
                                 </div>
                             </div>
                         </div>
-                    // ))
+                    </div>
                 )}
             </div>
         </>
