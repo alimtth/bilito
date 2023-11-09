a615f49ffd3940b4d684c926e660e980aa03278f
ali <alimotahari565@gmail.com>
2023-11-09 04:28:58 +0330

    fix bug

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import router from "./routes/index.jsx";
-import { RouterProvider } from "react-router-dom";
+import { Router, RouterProvider } from "react-router-dom";
 import { AuthProvider } from './providers/AuthProvider';
 import { UserDataProvider } from './providers/UserDataProvider';
 
