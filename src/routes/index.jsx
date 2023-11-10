72eaa466469ab6909ecd4288abc4d12febb9d79d
Ali Motahari <alimotahari565@gmail.com>
2023-11-10 14:25:57 +0330

    add layout (#65)
    * add data pic ui
    
    * add day
    
    * clean code
    
    * change layout

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,7 +2,7 @@ import React, { Children } from "react";
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "@/pages/index.jsx";
 import NotFound from "@/pages/404/NotFound.jsx";
-import BaseLayout from "@/pages/BaseLayout/index.jsx";
+import BaseLayout from "@/layouts/index.jsx";
 import Login from "@/components/Auth/Login/Login";
 import Register from "@/components/Auth/Register/Register"
 import TickedList from "@/pages/PageTicketList/TickedList";
@@ -12,7 +12,6 @@ import About from "@/pages/about/About";
 import BaseLayoutAcc from "@/pages/AccountPage/BaseLayoutsAccount/BaseLayoutAcc";
 import UserData from "@/pages/AccountPage/UserData/UserData";
 import EditUser from "@/pages/AccountPage/EditUser/EditUser";
-import EditUserData from "@/pages/AccountPage/EditUserData/EditUserData";
 import MyTicket from "@/pages/AccountPage/MyTicket/MyTicket";
 
 import FormListe from "@/pages/PageFormList/FormList";
