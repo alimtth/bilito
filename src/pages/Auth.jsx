46586dd2b60a375f71f81a82e57bc48a1f98d3cc
ali <alimotahari565@gmail.com>
2023-11-10 15:13:52 +0330

    add empty func eslint

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,5 +1,5 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
-import { Link } from "react-router-dom";
 import Button from '../components/Ui/Button';
 import InputTextField from '@/components/Ui/InputTextField';
 
