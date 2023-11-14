fcc52efa6e63c53eb8b336a2c16bfdc46af730f5
Ali Motahari <alimotahari565@gmail.com>
2023-11-14 15:50:25 +0330

    add massege

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -296,8 +296,17 @@ export const Payment = () => {
                 )}
               </div>
             </div>
+            <div className="flex justify-center">
+            <div className="w-2/3 bg-yellow-50 text-yellow-700 flex flex-col justify-center items-center py-10 rounded-lg mt-10 ">
+              <div className="">
+                پرداخت شما ناموفق بود، اگر هزینه بلیط از حسابتان کسر شده ظرف مدت
+                72 ساعت به حساب شما برمی گردد
+              </div>
+              <div className="mt-9">شماره پیگیری تراکنش: 123456789123</div>
+            </div>
+            </div>
             <div className="flex justify-center mt-9 gap-8">
-              <Link to={"/form/confirm-buy"}>
+              <Link to={'/form/confirm-buy'}>
                 <Buttons variant="fill" size="xxls">
                   <img src={wallet} alt="" />
                   پرداخت مجدد
@@ -306,9 +315,9 @@ export const Payment = () => {
             </div>
           </div>
         )}
-              <Buttons onClick={hanldTest} variant='fill'>
-                  <img src={wallet} alt="" />
-                </Buttons>
+        <Buttons onClick={hanldTest} variant="fill">
+          <img src={wallet} alt="" />
+        </Buttons>
       </div>
     </div>
   )
