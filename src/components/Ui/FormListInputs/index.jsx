3a4a9ad282d13f894f527caf7eeb5fd749833073
matin-mahabadi <144549212+matin-mahabadi@users.noreply.github.com>
2023-11-24 01:38:01 +0330

    Responsive form

--- src/components/Ui/FormListInputs/index.jsx
+++ src/components/Ui/FormListInputs/index.jsx
@@ -140,60 +140,171 @@ function FormListInputs({ isAdult, passenger, onUpdate }) {
         <span className="basis-[60%]">{adult ? 'بزرگسال' : 'کودک'}</span>
       </div>
       <form onSubmit={handleSubmit(onFormSubmit)}>
-        <div className="px-0 w-full gap-3 justify-center flex flex-col lg:flex lg:flex-row items-center">
-          <InputTextField
-            name="latin_name"
-            size={'slx'}
-            type="text"
-            value={passenger.latin_name || ''}
-            onChange={handleChange}
-          >
-            نام لاتین
-          </InputTextField>
-          {errors.firstName && (
-            <p className="text-red-500 text-sm mt-1">
-              {errors.firstName.message}
-            </p>
-          )}
-          <InputTextField
-            size={'slx'}
-            name="latin_last_name"
-            type="text"
-            value={passenger.latin_last_name || ''}
-            onChange={handleChange}
+        <div className="px-0 w-full gap-3">
+          <div className='gap-3 flex flex-col lg:flex-row items-center '>
+            <InputTextField
+              name="latin_name"
+              size={'slx'}
+              type="text"
+              value={passenger.latin_name || ''}
+              onChange={handleChange}
             >
-            نام خانوادگی
-          </InputTextField>
-          <InputTextField size={'slx'}>جنسیت</InputTextField>
+              نام لاتین
+            </InputTextField>
+            {errors.firstName && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.firstName.message}
+              </p>
+            )}
+            <InputTextField
+              size={'slx'}
+              name="latin_last_name"
+              type="text"
+              value={passenger.latin_last_name || ''}
+              onChange={handleChange}
+            >
+              نام خانوادگی
+            </InputTextField>
+            <InputTextField size={'slx'}>جنسیت</InputTextField>
+          </div>
+          <div className='flex flex-col justify-end items-center lg:fle lg:items-start'>
+            <div className="my-10 rounded-lg gap-2">
+              <div>تاریخ تولد (شمسی)</div>
+              <div className={'flex'}>
+                <div className="relative border border-gray-500 w-40 px-4 rounded-r-lg">
+                  <div
+                    className="flex items-center justify-between py-4"
+                    onClick={toggleDropdownDay}
+                  >
+                    <input
+                      readOnly
+                      type={'text'}
+                      className="w-1/2 focus:outline-none caret-transparent"
+                      value={dayBirthValue}
+                    />
+
+                    <IoIosArrowDown />
+                  </div>
+                  {dayDropdown && (
+                    <div className="flex flex-wrap w-40 bg-white shadow-lg gap-2 py-2 rounded-lg px-3 absolute z-10 -mr-4">
+                      <ul className="h-[300px] w-full overflow-y-scroll scroll-bar">
+                        {[...Array(31)].map((_, index) => (
+                          <li
+                            key={index + 1}
+                            value={index + 1}
+                            onClick={handleDay}
+                            className={`py-1 px-2 flex-shrink-0 cursor-pointer hover:bg-gray-10 hover:rounded-md ${selectedDay
+                              ? index === 'bg-blue-500 text-white'
+                              : ''
+                              }`}
+                          >
+                            {index + 1}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  )}
+                </div>
+                <div className="relative border border-gray-500 w-40 px-4">
+                  <div
+                    onClick={toggleDropdownMonth}
+                    className="flex items-center justify-between py-4"
+                  >
+                    <input
+                      readOnly
+                      type={'text'}
+                      className="w-1/2 focus:outline-none caret-transparent"
+                      value={monthValue}
+                    />
+                    <IoIosArrowDown />
+                  </div>
+                  {monthDropdown && (
+                    <div className="flex flex-wrap w-40 bg-white shadow-lg gap-2 py-2 rounded-lg px-3 absolute z-10 -mr-4">
+                      <ul className="h-[300px] w-full overflow-y-scroll  scroll-bar">
+                        {months.map((items) => (
+                          <li
+                            key={items.id}
+                            value={items.value}
+                            onClick={handleMonth}
+                            className={`py-1 px-2 flex-shrink-0 cursor-pointer hover:bg-gray-10 hover:rounded-md `}
+                          >
+                            {items.title}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  )}
+                </div>
+                <div className="relative border border-gray-500 w-40 px-4 rounded-l-lg">
+                  <div
+                    onClick={toggleDropdownYear}
+                    className="flex items-center justify-between py-4"
+                  >
+                    <input
+                      readOnly
+                      type={'text'}
+                      className="w-1/2 focus:outline-none caret-transparent"
+                      value={yearValue}
+                    />
+                    <IoIosArrowDown />
+                  </div>
+                  {yearDropdown && (
+                    <div className="flex flex-wrap w-40 bg-white shadow-lg gap-2 py-2 rounded-lg px-3 absolute z-10 -mr-4">
+                      <ul className="h-[300px] w-full overflow-y-scroll  scroll-bar">
+                        {birthYears.map((items, index) => (
+                          <li
+                            key={index + 1}
+                            value={items.title}
+                            onClick={handleYear}
+                            className={`py-1 px-2 flex-shrink-0 cursor-pointer hover:bg-gray-10 hover:rounded-md ${selectedDay
+                              ? index === 'bg-blue-500 text-white'
+                              : ''
+                              }`}
+                          >
+                            {items.title}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
-          <div className="my-10 rounded-lg gap-2 flex flex-col items-center">
-            <div>تاریخ تولد (شمسی)</div>
+          </div>
+        </div>
+
+        <div className="my-7 pt-4 px-0 w-full gap-3">
+          <div className='gap-3 flex flex-col lg:flex-row items-center'>
+          <InputTextField size={'slx'}>ملیت</InputTextField>
+          <InputTextField size={'slx'}>کد ملی</InputTextField>
+          <InputTextField size={'slx'}>شماره پاسپورت</InputTextField>
+          </div>
+          <div className="mt-10 rounded-lg gap-2  flex flex-col justify-end items-center lg:fle lg:items-start">
+            <div>تاریخ انقضا پاسپورت (شمسی)</div>
             <div className={'flex'}>
               <div className="relative border border-gray-500 w-40 px-4 rounded-r-lg">
                 <div
                   className="flex items-center justify-between py-4"
-                  onClick={toggleDropdownDay}
+                  onClick={toggleDropdownExpireDay}
                 >
                   <input
-                    readOnly
-                    type={'text'}
                     className="w-1/2 focus:outline-none caret-transparent"
-                    value={dayBirthValue}
+                    value={expireDayValue}
                   />
 
                   <IoIosArrowDown />
                 </div>
-                {dayDropdown && (
+                {expireDayDropdown && (
                   <div className="flex flex-wrap w-40 bg-white shadow-lg gap-2 py-2 rounded-lg px-3 absolute z-10 -mr-4">
                     <ul className="h-[300px] w-full overflow-y-scroll scroll-bar">
                       {[...Array(31)].map((_, index) => (
                         <li
                           key={index + 1}
                           value={index + 1}
-                          onClick={handleDay}
+                          onClick={handleExpireDay}
                           className={`py-1 px-2 flex-shrink-0 cursor-pointer hover:bg-gray-10 hover:rounded-md ${selectedDay
-                              ? index === 'bg-blue-500 text-white'
-                              : ''
+                            ? index === 'bg-blue-500 text-white'
+                            : ''
                             }`}
                         >
                           {index + 1}
@@ -205,25 +316,23 @@ function FormListInputs({ isAdult, passenger, onUpdate }) {
               </div>
               <div className="relative border border-gray-500 w-40 px-4">
                 <div
-                  onClick={toggleDropdownMonth}
+                  onClick={toggleDropdownExpireMonth}
                   className="flex items-center justify-between py-4"
                 >
                   <input
-                    readOnly
-                    type={'text'}
                     className="w-1/2 focus:outline-none caret-transparent"
-                    value={monthValue}
+                    value={expireMonthValue}
                   />
                   <IoIosArrowDown />
                 </div>
-                {monthDropdown && (
+                {expireMonthDropdown && (
                   <div className="flex flex-wrap w-40 bg-white shadow-lg gap-2 py-2 rounded-lg px-3 absolute z-10 -mr-4">
                     <ul className="h-[300px] w-full overflow-y-scroll  scroll-bar">
                       {months.map((items) => (
                         <li
                           key={items.id}
                           value={items.value}
-                          onClick={handleMonth}
+                          onClick={handleExpireMonth}
                           className={`py-1 px-2 flex-shrink-0 cursor-pointer hover:bg-gray-10 hover:rounded-md `}
                         >
                           {items.title}
@@ -235,28 +344,26 @@ function FormListInputs({ isAdult, passenger, onUpdate }) {
               </div>
               <div className="relative border border-gray-500 w-40 px-4 rounded-l-lg">
                 <div
-                  onClick={toggleDropdownYear}
+                  onClick={toggleDropdownExpireYear}
                   className="flex items-center justify-between py-4"
                 >
                   <input
-                    readOnly
-                    type={'text'}
                     className="w-1/2 focus:outline-none caret-transparent"
-                    value={yearValue}
+                    value={expireYearValue}
                   />
                   <IoIosArrowDown />
                 </div>
-                {yearDropdown && (
+                {expireYearDropdown && (
                   <div className="flex flex-wrap w-40 bg-white shadow-lg gap-2 py-2 rounded-lg px-3 absolute z-10 -mr-4">
                     <ul className="h-[300px] w-full overflow-y-scroll  scroll-bar">
-                      {birthYears.map((items, index) => (
+                      {expireYears.map((items, index) => (
                         <li
                           key={index + 1}
                           value={items.title}
-                          onClick={handleYear}
+                          onClick={handleExpireYear}
                           className={`py-1 px-2 flex-shrink-0 cursor-pointer hover:bg-gray-10 hover:rounded-md ${selectedDay
-                              ? index === 'bg-blue-500 text-white'
-                              : ''
+                            ? index === 'bg-blue-500 text-white'
+                            : ''
                             }`}
                         >
                           {items.title}
@@ -266,107 +373,6 @@ function FormListInputs({ isAdult, passenger, onUpdate }) {
                   </div>
                 )}
               </div>
-            </div>
-          </div>
-
-        <div className="my-7 pt-4 px-0 w-full gap-3 flex flex-col lg:flex lg:flex-row items-center justify-center">
-          <InputTextField size={'slx'}>ملیت</InputTextField>
-          <InputTextField size={'slx'}>کد ملی</InputTextField>
-          <InputTextField size={'slx'}>شماره پاسپورت</InputTextField>
-                </div>
-        <div className="mt-10 flex flex-col rounded-lg gap-2 items-center">
-          <div>تاریخ انقضا پاسپورت (شمسی)</div>
-          <div className={'flex'}>
-            <div className="relative border border-gray-500 w-40 px-4 rounded-r-lg">
-              <div
-                className="flex items-center justify-between py-4"
-                onClick={toggleDropdownExpireDay}
-                >
-                <input
-                  className="w-1/2 focus:outline-none caret-transparent"
-                  value={expireDayValue}
-                  />
-
-                <IoIosArrowDown />
-              </div>
-              {expireDayDropdown && (
-                <div className="flex flex-wrap w-40 bg-white shadow-lg gap-2 py-2 rounded-lg px-3 absolute z-10 -mr-4">
-                  <ul className="h-[300px] w-full overflow-y-scroll scroll-bar">
-                    {[...Array(31)].map((_, index) => (
-                      <li
-                      key={index + 1}
-                      value={index + 1}
-                      onClick={handleExpireDay}
-                      className={`py-1 px-2 flex-shrink-0 cursor-pointer hover:bg-gray-10 hover:rounded-md ${selectedDay
-                        ? index === 'bg-blue-500 text-white'
-                        : ''
-                      }`}
-                      >
-                        {index + 1}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
-            </div>
-            <div className="relative border border-gray-500 w-40 px-4">
-              <div
-                onClick={toggleDropdownExpireMonth}
-                className="flex items-center justify-between py-4"
-                >
-                <input
-                  className="w-1/2 focus:outline-none caret-transparent"
-                  value={expireMonthValue}
-                  />
-                <IoIosArrowDown />
-              </div>
-              {expireMonthDropdown && (
-                <div className="flex flex-wrap w-40 bg-white shadow-lg gap-2 py-2 rounded-lg px-3 absolute z-10 -mr-4">
-                  <ul className="h-[300px] w-full overflow-y-scroll  scroll-bar">
-                    {months.map((items) => (
-                      <li
-                      key={items.id}
-                      value={items.value}
-                      onClick={handleExpireMonth}
-                      className={`py-1 px-2 flex-shrink-0 cursor-pointer hover:bg-gray-10 hover:rounded-md `}
-                      >
-                        {items.title}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
-            </div>
-            <div className="relative border border-gray-500 w-40 px-4 rounded-l-lg">
-              <div
-                onClick={toggleDropdownExpireYear}
-                className="flex items-center justify-between py-4"
-                >
-                <input
-                  className="w-1/2 focus:outline-none caret-transparent"
-                  value={expireYearValue}
-                  />
-                <IoIosArrowDown />
-              </div>
-              {expireYearDropdown && (
-                <div className="flex flex-wrap w-40 bg-white shadow-lg gap-2 py-2 rounded-lg px-3 absolute z-10 -mr-4">
-                  <ul className="h-[300px] w-full overflow-y-scroll  scroll-bar">
-                    {expireYears.map((items, index) => (
-                      <li
-                      key={index + 1}
-                      value={items.title}
-                      onClick={handleExpireYear}
-                      className={`py-1 px-2 flex-shrink-0 cursor-pointer hover:bg-gray-10 hover:rounded-md ${selectedDay
-                        ? index === 'bg-blue-500 text-white'
-                        : ''
-                      }`}
-                      >
-                        {items.title}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
             </div>
           </div>
         </div>
