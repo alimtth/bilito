bd4af6e34931dd0df6635f04f4e86c46b7a598d4
Ali Motahari <alimotahari565@gmail.com>
2023-11-23 10:47:43 +0330

    edit bime

--- src/pages/Insurance/index.jsx
+++ src/pages/Insurance/index.jsx
@@ -26,209 +26,223 @@ function Insurace() {
 
   return (
     <>
-      <HomePageScreen />
-      <div className="p-8">
-        <div className="w-full h-full flex flex-col lg:flex-row justify-between gap-8 items-center md:sm:shadow-2xl sm:shadow-none lg:w-fit m-auto bg-white lg:items-start rounded-xl p-6">
-          <InputTextField value={''} size="sm" className={'sm:px-44 lg:px-0 '}>
-            کشور مبدا
-          </InputTextField>
-          <InputTextField value={''} size="sm" className={'sm:px-44 lg:px-0 '}>
-            مدت سفر
-          </InputTextField>
-          <InputTextField value={''} size="sm" className={'sm:px-44 lg:px-0 '}>
-            مسافران 
-          </InputTextField>
-          <Button
-            variant="fill"
-            size="xl"
-            className={' sm:px-56 lg:px-3'}
-            type="submit"
-          >
-            <img src={searchIcon} />
-            جستجو
-          </Button>
+        <HomePageScreen />
+      <div id="container">
+        <div className="p-8">
+          <div className="w-full h-full flex flex-col lg:flex-row justify-between gap-8 items-center md:sm:shadow-2xl sm:shadow-none lg:w-fit m-auto bg-white lg:items-start rounded-xl p-6">
+            <InputTextField
+              value={''}
+              size="sm"
+              className={'sm:px-44 lg:px-0 '}
+            >
+              کشور مبدا
+            </InputTextField>
+            <InputTextField
+              value={''}
+              size="sm"
+              className={'sm:px-44 lg:px-0 '}
+            >
+              مدت سفر
+            </InputTextField>
+            <InputTextField
+              value={''}
+              size="sm"
+              className={'sm:px-44 lg:px-0 '}
+            >
+              مسافران
+            </InputTextField>
+            <Button
+              variant="fill"
+              size="xl"
+              className={' sm:px-56 lg:px-3'}
+              type="submit"
+            >
+              <img src={searchIcon} />
+              جستجو
+            </Button>
+          </div>
         </div>
-      </div>
-      <div className="p-8">
-        <p className="font-bold">بیمه مسافرتی چیست؟</p>
-        <p>
-          بیمه مسافرتی (Travel Insurance) یک نوع بیمه است که برای پوشش هزینه‌ها
-          و خسارات مرتبط با سفرهای بین‌المللی یا داخلی ارائه می‌شود. این بیمه
-          معمولاً توسط شرکت‌های بیمه عرضه می‌شود و شامل تعدادی پوشش است که
-          می‌تواند شامل پوشش هزینه‌های پزشکی اضطراری، پوشش لغو سفر، پوشش بیمه
-          مسافرتی برای خسارت اموال، مسئولیت مدنی و پوشش تأخیرات و لغو پرواز
-          باشد. یکی از اصلی‌ترین پوشش‌های بیمه مسافرتی پوشش هزینه‌های پزشکی است.
-          اگر در سفر به بیماری یا حادثه‌ای برخورد کنید و نیاز به درمان داشته
-          باشید، بیمه مسافرتی ممکن است هزینه‌های مرتبط با ویزیت پزشک، بستری در
-          بیمارستان، داروها و معالجه‌های دیگر را پوشش دهد
-        </p>
-        <div>
-          <p className="font-bold py-8">خدمات بیمه مسافرتی</p>
+        <div className="p-8 flex flex-col gap-4">
+          <p className="font-bold">بیمه مسافرتی چیست؟</p>
           <p>
-            خدمات بیمه مسافرتی شامل مجموعه‌ای از خدماتی هستند که توسط شرکت‌های
-            بیمه به بیمه‌گزاران ارائه می‌شوند. این خدمات در ارتباط با موارد
-            مختلفی از جمله پوشش‌ها، مشاوره‌ها و خدمات اضافی مرتبط با سفرها ارائه
-            می‌شود. به طور کلی، خدمات بیمه مسافرتی عبارتند از:
+            بیمه مسافرتی (Travel Insurance) یک نوع بیمه است که برای پوشش
+            هزینه‌ها و خسارات مرتبط با سفرهای بین‌المللی یا داخلی ارائه می‌شود.
+            این بیمه معمولاً توسط شرکت‌های بیمه عرضه می‌شود و شامل تعدادی پوشش
+            است که می‌تواند شامل پوشش هزینه‌های پزشکی اضطراری، پوشش لغو سفر،
+            پوشش بیمه مسافرتی برای خسارت اموال، مسئولیت مدنی و پوشش تأخیرات و
+            لغو پرواز باشد. یکی از اصلی‌ترین پوشش‌های بیمه مسافرتی پوشش
+            هزینه‌های پزشکی است. اگر در سفر به بیماری یا حادثه‌ای برخورد کنید و
+            نیاز به درمان داشته باشید، بیمه مسافرتی ممکن است هزینه‌های مرتبط با
+            ویزیت پزشک، بستری در بیمارستان، داروها و معالجه‌های دیگر را پوشش دهد
           </p>
-        </div>
-        <div className="flex flex-col gap-3 pt-2">
-          <div className="flex flex-row items-center gap-3">
-            <img src={tableReportMedical} alt="table" />
-            <div>
-              <span className="font-bold">هزینه های پزشکی:</span>
-              <span>
-                هزینه مراقبت پزشکی و بستری در بیمارستان برای درمان بیماری‌ها یا
-                حوادث و صدمات مختلف.
-              </span>
-            </div>
-          </div>
-
-          <div className="flex flex-row items-center gap-3">
-            <img src={tablerDental} alt="tabler" />
-            <div>
-              <span className="font-bold">هزینه های دندانپزشکی:</span>
-              <span>
-                درمان ضروری برای درمان عفونت شدید، دندان درد، کشیدن دندان و...
-              </span>
-            </div>
-          </div>
-          <div className="flex flex-row items-center gap-3">
-            <img src={plus} alt="airplan" />
-            <div>
-              <span className="font-bold">جبران هزینه دارو:</span>
-              <span>
-                در صورت مفقود شدن داروهای همراه مسافر (به شرط ضروری بودن مصرف
-                آن‌ها) و ارسال مجدد داروهای مورد نیاز بیمه‌شده.
-              </span>
-            </div>
-          </div>
-          <div className="flex flex-row items-center gap-3">
-            <img src={airplan} alt="airplan" />
-            <div>
-              <span className="font-bold">هزینه‌های بازگشت: </span>
-              <span>
-                در صورت ابتلا به بیماری یا وقوع حادثه‌ای که بیش از 10 روز نیاز
-                به بستری شدن در بیمارستان داشته باشد.
-              </span>
-            </div>
-          </div>
-          <div className="flex flex-row items-center gap-3">
-            <img src={cardTick} alt="cardtick" />
-            <div>
-              <span className="font-bold">جبران خسارت اموال:</span>
-              <span>
-                بیمهه مسافرتی می‌تواند در صورت خسارت، سرقت یا از دست رفتن وسایل
-                شخصی و اموال شما در سفر تأمین کند
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col gap-4">
-          <div className="py-8">
-            <p className="font-bold">مبلغ بیمه مسافرتی</p>
-          </div>
           <div>
+            <p className="font-bold py-8">خدمات بیمه مسافرتی</p>
             <p>
-              قیمت بیمه مسافرتی بر اساس عوامل متعددی تعیین می‌شود. در زیر به
-              برخی از اصلی‌ترین عواملی که بر قیمت بیمه مسافرتی تأثیر می‌گذارند،
-              اشاره شده است.
+              خدمات بیمه مسافرتی شامل مجموعه‌ای از خدماتی هستند که توسط شرکت‌های
+              بیمه به بیمه‌گزاران ارائه می‌شوند. این خدمات در ارتباط با موارد
+              مختلفی از جمله پوشش‌ها، مشاوره‌ها و خدمات اضافی مرتبط با سفرها
+              ارائه می‌شود. به طور کلی، خدمات بیمه مسافرتی عبارتند از:
             </p>
           </div>
-          <div className="flex flex-row items-center gap-3">
-            <div>
-              <span className="font-bold">مقصد سفر: </span>
-              <span>
-                مقصد سفر یکی از عوامل اصلی در تعیین قیمت بیمه مسافرتی است. برخی
-                مقاصد سفر به دلیل خطرات بیشتر، هزینه درمان بالاتر یا موارد دیگر،
-                قیمت بیمه بیشتری دارند.
-              </span>
-            </div>
-          </div>
-          <div className="flex flex-row items-center gap-3">
-            <div>
-              <span className="font-bold">مدت زمان سفر:</span>
-
-              <span>
-                مدت زمان سفر نیز تأثیر قابل توجهی در قیمت بیمه دارد. معمولاً
-                هرچه مدت زمان سفر بیشتر باشد، قیمت بیمه مسافرتی نیز بالاتر خواهد
-                بود
-              </span>
-            </div>
-          </div>
-          <div className="flex flex-row items-center gap-3">
-            <div>
-              <span className="font-bold">سن و تعداد مسافران:</span>
-
-              <span>
-                سن و تعداد مسافران ممکن است نیز تأثیرگذار باشند. برخی
-                بیمه‌گزاران در سنین بالا ممکن است قیمت بیمه بیشتری داشته باشند.
-                همچنین، در صورتی که تعداد مسافران بیشتر باشد، قیمت بیمه ممکن است
-                بیشتر شود.
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className="bg-blue-100 rounded-lg sm:p-8 flex flex-col md:flex-row">
-          <table className=" w-full h-full border border-blue-300 rounded-lg ">
-            <tr className="bg-blue-250 flex flex-row items-center p-3">
-              <th className="basis-2/12">عوامل موثر در تعیین قیمت</th>
-              <th className="basis-10/12">
-                نحوه محاسبه در فرمول تعیین حق بیمه
-              </th>
-            </tr>
-            <tr className="flex flex-row p-3 font-normal">
-              <td className="basis-2/12">سن مسافر</td>
-              <td className="basis-10/12">
-                تا ۱۲ سال/ ۱۳ تا ۶۵ سال/ 66 تا 70 سال/ ۷۱ تا ۷۵ سال/ ۷۶ تا ۸۰
-                سال
-              </td>
-            </tr>
-            <tr className="flex flex-row p-3 font-normal ">
-              <td className="basis-2/12">مدت سفر</td>
-              <td className="basis-10/12">
-                ۱ تا ۷ روز/ ۸ تا ۱۵ روز/ ۱۶ تا ۲۳ روز/ ۲۴ تا ۳۱ روز/ ۳۲ تا ۴۵
-                روز/ ۶۳ تا ۹۲ روز/ ۶ ماهه/ ۱ ساله
-              </td>
-            </tr>
-            <tr className="flex flex-row p-3 font-normal">
-              <td className="basis-2/12">مقصد سفر</td>
-              <td className="basis-10/12">بر اساس مناطق جغرافیایی مختلف</td>
-            </tr>
-          </table>
-          <div className="flex items-center justify-center">
-            <img src={airplanTable} alt="" />
-          </div>
-        </div>
-        <div className="py-8 w-full bg-white rounded-b-[80px]">
-          <h1 className="text-xl font-bold mb-8">سوالات متداول</h1>
-          {insuraceAccordion.map((items) => {
-            return (
-              <div key={items.id} className='border border-x-gray-10'>
-                <Accordion
-                  expanded={expanded === items.id}
-                  onChange={handleChange(items.id)}
-                >
-                  <AccordionSummary
-                    aria-controls={`${items.id}d-content`}
-                    id={`${items.id}d-header`}
-                  >
-                    <div
-                      className={`${
-                        expanded === items.id ? 'text-blue-500' : ''
-                      }`}
-                    >
-                      <Typography className="font1">{items.title}</Typography>
-                    </div>
-                  </AccordionSummary>
-                  <AccordionDetails>
-                    <Typography className="font1">
-                      {items.description}
-                    </Typography>
-                  </AccordionDetails>
-                </Accordion>
+          <div className="flex flex-col gap-3 pt-2">
+            <div className="flex flex-row items-center gap-3">
+              <img src={tableReportMedical} alt="table" />
+              <div>
+                <span className="font-bold">هزینه های پزشکی:</span>
+                <span>
+                  هزینه مراقبت پزشکی و بستری در بیمارستان برای درمان بیماری‌ها
+                  یا حوادث و صدمات مختلف.
+                </span>
               </div>
-            )
-          })}
+            </div>
+
+            <div className="flex flex-row items-center gap-3">
+              <img src={tablerDental} alt="tabler" />
+              <div>
+                <span className="font-bold">هزینه های دندانپزشکی:</span>
+                <span>
+                  درمان ضروری برای درمان عفونت شدید، دندان درد، کشیدن دندان و...
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-row items-center gap-3">
+              <img src={plus} alt="airplan" />
+              <div>
+                <span className="font-bold">جبران هزینه دارو:</span>
+                <span>
+                  در صورت مفقود شدن داروهای همراه مسافر (به شرط ضروری بودن مصرف
+                  آن‌ها) و ارسال مجدد داروهای مورد نیاز بیمه‌شده.
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-row items-center gap-3">
+              <img src={airplan} alt="airplan" />
+              <div>
+                <span className="font-bold">هزینه‌های بازگشت: </span>
+                <span>
+                  در صورت ابتلا به بیماری یا وقوع حادثه‌ای که بیش از 10 روز نیاز
+                  به بستری شدن در بیمارستان داشته باشد.
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-row items-center gap-3">
+              <img src={cardTick} alt="cardtick" />
+              <div>
+                <span className="font-bold">جبران خسارت اموال:</span>
+                <span>
+                  بیمهه مسافرتی می‌تواند در صورت خسارت، سرقت یا از دست رفتن
+                  وسایل شخصی و اموال شما در سفر تأمین کند
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4">
+            <div className="py-8">
+              <p className="font-bold">مبلغ بیمه مسافرتی</p>
+            </div>
+            <div>
+              <p>
+                قیمت بیمه مسافرتی بر اساس عوامل متعددی تعیین می‌شود. در زیر به
+                برخی از اصلی‌ترین عواملی که بر قیمت بیمه مسافرتی تأثیر
+                می‌گذارند، اشاره شده است.
+              </p>
+            </div>
+            <div className="flex flex-row items-center gap-3">
+              <div>
+                <span className="font-bold">مقصد سفر: </span>
+                <span>
+                  مقصد سفر یکی از عوامل اصلی در تعیین قیمت بیمه مسافرتی است.
+                  برخی مقاصد سفر به دلیل خطرات بیشتر، هزینه درمان بالاتر یا
+                  موارد دیگر، قیمت بیمه بیشتری دارند.
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-row items-center gap-3">
+              <div>
+                <span className="font-bold">مدت زمان سفر:</span>
+
+                <span>
+                  مدت زمان سفر نیز تأثیر قابل توجهی در قیمت بیمه دارد. معمولاً
+                  هرچه مدت زمان سفر بیشتر باشد، قیمت بیمه مسافرتی نیز بالاتر
+                  خواهد بود
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-row items-center gap-3">
+              <div>
+                <span className="font-bold">سن و تعداد مسافران:</span>
+
+                <span>
+                  سن و تعداد مسافران ممکن است نیز تأثیرگذار باشند. برخی
+                  بیمه‌گزاران در سنین بالا ممکن است قیمت بیمه بیشتری داشته
+                  باشند. همچنین، در صورتی که تعداد مسافران بیشتر باشد، قیمت بیمه
+                  ممکن است بیشتر شود.
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="bg-blue-100 rounded-lg sm:p-8 flex flex-col md:flex-row">
+            <table className=" w-full h-full border border-blue-300 rounded-lg ">
+              <tr className="bg-blue-250 flex flex-row items-center p-3">
+                <th className="basis-2/12">عوامل موثر در تعیین قیمت</th>
+                <th className="basis-10/12">
+                  نحوه محاسبه در فرمول تعیین حق بیمه
+                </th>
+              </tr>
+              <tr className="flex flex-row p-3 font-normal">
+                <td className="basis-2/12">سن مسافر</td>
+                <td className="basis-10/12">
+                  تا ۱۲ سال/ ۱۳ تا ۶۵ سال/ 66 تا 70 سال/ ۷۱ تا ۷۵ سال/ ۷۶ تا ۸۰
+                  سال
+                </td>
+              </tr>
+              <tr className="flex flex-row p-3 font-normal ">
+                <td className="basis-2/12">مدت سفر</td>
+                <td className="basis-10/12">
+                  ۱ تا ۷ روز/ ۸ تا ۱۵ روز/ ۱۶ تا ۲۳ روز/ ۲۴ تا ۳۱ روز/ ۳۲ تا ۴۵
+                  روز/ ۶۳ تا ۹۲ روز/ ۶ ماهه/ ۱ ساله
+                </td>
+              </tr>
+              <tr className="flex flex-row p-3 font-normal">
+                <td className="basis-2/12">مقصد سفر</td>
+                <td className="basis-10/12">بر اساس مناطق جغرافیایی مختلف</td>
+              </tr>
+            </table>
+            <div className="flex items-center justify-center">
+              <img src={airplanTable} alt="" />
+            </div>
+          </div>
+          <div className="py-8 w-full bg-white rounded-b-[80px]">
+            <h1 className="text-xl font-bold mb-8">سوالات متداول</h1>
+            {insuraceAccordion.map((items) => {
+              return (
+                <div key={items.id} className="border border-x-gray-10">
+                  <Accordion
+                    expanded={expanded === items.id}
+                    onChange={handleChange(items.id)}
+                  >
+                    <AccordionSummary
+                      aria-controls={`${items.id}d-content`}
+                      id={`${items.id}d-header`}
+                    >
+                      <div
+                        className={`${
+                          expanded === items.id ? 'text-blue-500' : ''
+                        }`}
+                      >
+                        <Typography className="font1">{items.title}</Typography>
+                      </div>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <Typography className="font1">
+                        {items.description}
+                      </Typography>
+                    </AccordionDetails>
+                  </Accordion>
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
     </>
